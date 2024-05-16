41b37250743b04863eed517f0eb3975aadd55d5c
Rishabh Shukla <shuklarishabh618@gmail.com>
2024-05-16 10:06:06 +0530

    regular commit

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import NewsItem from './NewsItem';
 
 
 export class News extends Component {
-  static propTypes = {
-
-  }
 
   render() {
     return (
-      <div>
-        This is news compoen
-        <NewsItem />
+      <div className="container my-3">
+        <h2>NewsHorse - Top Headlines</h2>
+        <div className="row">
+          <div className="col-md-4">
+         <NewsItem title="myTitle" description="myDescription"/>
+
+          </div>
+          <div className="col-md-4">
+          <NewsItem title="myTitle" description="myDescription"/>
+
+          </div>
+          <div className="col-md-4">
+          <NewsItem title="myTitle" description="myDescription"/>
+
+          </div>
+        </div>
       </div>
     )
   }
--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -1,19 +1,23 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
 
 export class NewsItem extends Component {
-  static propTypes = {
-
-  }
-
   render() {
+    let { title, description } = this.props;
     return (
       <div>
-        this is a news item
-       
+        <div className="card" style={{ width: "18rem" }}>
+          <img src="https://www.nationalreview.com/wp-content/uploads/2022/08/ron-desantis-florida02.jpg?fit=2057%2C1200" className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <a href="/newsdetails" className="btn btn-primary">
+              Go somewhere
+            </a>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
 
-export default NewsItem
+export default NewsItem;
