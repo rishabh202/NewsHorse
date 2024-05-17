47dbd5722dff57e0546bfc5246f3d0237f5ac056
Rishabh Shukla <shuklarishabh618@gmail.com>
2024-05-17 21:48:29 +0530

    regular commit: updated ui

--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -6,7 +6,7 @@ export class NewsItem extends Component {
     return (
       <div>
         <div className="card" style={{ width: "18rem" }}>
-          <img src="https://www.nationalreview.com/wp-content/uploads/2022/08/ron-desantis-florida02.jpg?fit=2057%2C1200" className="card-img-top" alt="..." />
+          <img src="https://www.nationalreview.com/wp-content/uploads/2022/08/ron-desantis-florida02.jpg?fit=2057%2C1200" className="card-img-top" alt="" />
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
