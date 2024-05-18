93ce481ac96a144d32b2817daa54f59e5057d94b
Rishabh Shukla <shuklarishabh618@gmail.com>
2024-05-18 09:57:29 +0530

    regular commit, updated ui

--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -1,17 +1,18 @@
 import React, { Component } from "react";
 
 export class NewsItem extends Component {
+
   render() {
-    let { title, description } = this.props;
+    let { title, description, imageUrl, newsUrl } = this.props;
     return (
-      <div>
+      <div className="my-3">
         <div className="card" style={{ width: "18rem" }}>
-          <img src="https://www.nationalreview.com/wp-content/uploads/2022/08/ron-desantis-florida02.jpg?fit=2057%2C1200" className="card-img-top" alt="" />
+          <img src={imageUrl} className="card-img-top" alt="" />
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
-            <a href="/newsdetails" className="btn btn-primary">
-              Go somewhere
+            <a href="/newsdetails/" className="btn btn-primary btn-sm">
+              Read More
             </a>
           </div>
         </div>
